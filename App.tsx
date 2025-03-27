fd6f66e6a230dad11345a09e862d923bcc46e553
rajkumar20197 <139485876+rajkumar20197@users.noreply.github.com>
2025-03-26 22:35:33 -0700

    Add files via upload
    Level One

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+          <Task text={'Figma Design'} />
+          <Task text={'Work Shop'} />
+          <Task text={'Lindo'} />
+          <Task text={'React Native'} />
+          <Task text={'WorkOut'} />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 10,
+  }
+});
